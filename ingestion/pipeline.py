51d9dd8095270500fab081959bebc1396303b168
ryw27 <rywbook06@gmail.com>
2025-11-03 23:44:10 -0500

    Use database in pipeline

--- ingestion/pipeline.py
+++ ingestion/pipeline.py
@@ -1,9 +1,9 @@
 from fetcher import PaperFetcher
 from processor import PaperProcessor
-from typing import List, Dict
+from typing import List 
 from models import Chunk, Embedding, Paper
-import uuid
 import asyncio
+from db import Database
 
 # Placeholder for postgres/open search
 chunks: List[Chunk] = []
@@ -16,6 +16,8 @@ class Pipeline:
         self.fetcher = PaperFetcher()
         self.processor = PaperProcessor()
 
+        self.database = Database()
+
     async def ingest_papers(self, query: str, max_results: int = 10) -> None:
         # 1. Fetch papers and store in directory
         pdf_files = await self.fetcher.fetch_pdfs(query, max_results)
@@ -23,25 +25,28 @@ class Pipeline:
         # 2. Process them 
         for pdf in pdf_files:
             filename, metadata = pdf
-            papers.append(metadata)
+
+            self.database.insert_paper(metadata)
+
             text_chunks, embedded_chunks = await self.processor.process_pdf(filename)
+            for text, embed in zip(text_chunks, embedded_chunks):
+                self.database.insert_chunk(text)
+                self.database.insert_embedding(embed)
+                # chunk_id = uuid.uuid4()
 
-            for idx, (text, embed) in enumerate(zip(text_chunks, embedded_chunks)):
-                chunk_id = uuid.uuid4()
+                # chunks.append(Chunk(
+                #     arxiv_id=metadata.arxiv_id,
+                #     chunk_id=chunk_id,
+                #     chunk_index=idx,
+                #     chunk_text=text,
+                #     token_count=len(text)
+                # ))
 
-                chunks.append(Chunk(
-                    arxiv_id=metadata.arxiv_id,
-                    chunk_id=chunk_id,
-                    chunk_index=idx,
-                    chunk_text=text,
-                    token_count=len(text)
-                ))
-
-                vector_db.append(Embedding(
-                    chunk_id=chunk_id,
-                    embedding_id=uuid.uuid4(),
-                    embedding=embed
-                ))
+                # vector_db.append(Embedding(
+                #     chunk_id=chunk_id,
+                #     embedding_id=uuid.uuid4(),
+                #     embedding=embed
+                # ))
 
 if __name__ == "__main__":
     pipeline = Pipeline()
